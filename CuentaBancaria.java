ba74d9ce627677ff31c12e4491dcb13289ae55fd
Ainhoa Juarez Practica Git <ainhoajuarez@iesprofesorangelysern.es>
2023-03-25 11:37:46 +0100

    Commit para recursiva

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -2,7 +2,8 @@ class TestCuentaBancaria
 {
 	public static void main(String args[])
 	{
-		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
+		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100.0);
+		CuentaBancaria cuenta2= new CuentaBancaria("0123456789", "Ainhoa Juarez", 100.5);
 		int TipoIntereses;
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
